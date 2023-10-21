847f1e10e4cefede32cb42146c8e5060a00128c1
rivernine <rivernine@github.com>
2023-10-21 17:13:14 +0900

    ⚰️ Remove dead code

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      'https://webflow.co.kr',
-      'https://www.webflow.co.kr',
-      'http://webflow.co.kr',
-      'http://www.webflow.co.kr',
-      'http://124.48.192.106:5173'
+      *
     ],
   });
   app.setGlobalPrefix('api/v1');
   swaggerConfig(app);
-  await app.listen(8081);
+  await app.listen(8080);
 }
 bootstrap();
